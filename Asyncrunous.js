5e918a4251ef49d8bc0829318ed957fbb1d6ee9f
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-10-08 00:17:40 +0530

    Update Asyncrunous.js

--- Asyncrunous.js
+++ Asyncrunous.js
@@ -1,11 +1,1 @@
-/*
-Asynchronous Programming / Asynchronous Function
-*/
-function AsignTask2() {
-  console.log("Start of script");
-  setTimeout(function () {
-    console.log("First timeout completed");
-  }, 2000);
-  console.log("End of script");
-}
-AsignTask();
+
