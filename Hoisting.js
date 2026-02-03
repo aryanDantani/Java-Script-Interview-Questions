b40df78715728cdd82c644a80dac61f091b76975
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2026-02-03 23:30:51 +0530

    Document hoisting behavior in Hoisting.js
    Added summary of hoisting behavior for var, let, and const.

--- Hoisting.js
+++ Hoisting.js
@@ -1,3 +1,11 @@
+Summary of Hoisting Behavior
+Declaration Keyword 	Hoisted?	Initialized with undefined?	Accessible before declaration?
+var	Yes	Yes	Yes (value is undefined)
+let	Yes	No	No (ReferenceError due to TDZ)
+const	Yes	No	No (ReferenceError due to TDZ)
+
+
+
 GetName();
 // console.log(value);
 
@@ -6,3 +14,4 @@ GetName();
 function GetName() {
   console.log("Hello Aryan 👍");
 }
+
