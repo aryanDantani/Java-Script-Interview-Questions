172df6898722a50da4402c7bff72c0ec1acb5fc9
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2026-04-13 22:01:28 +0530

    Update Hoisting.js formatting and content

--- Hoisting.js
+++ Hoisting.js
@@ -1,17 +1,24 @@
 Summary of Hoisting Behavior
+
 Declaration Keyword 	Hoisted?	Initialized with undefined?	Accessible before declaration?
+  
 var	Yes	Yes	Yes (value is undefined)
+
 let	Yes	No	No (ReferenceError due to TDZ)
+
 const	Yes	No	No (ReferenceError due to TDZ)
 
 
 
 GetName();
+
 // console.log(value);
 
 // const value = 10;
 
 function GetName() {
+  
   console.log("Hello Aryan 👍");
+  
 }
 
